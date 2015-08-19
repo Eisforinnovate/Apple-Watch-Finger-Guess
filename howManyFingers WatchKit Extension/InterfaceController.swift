f4db6070bcaac6b4e4547a962a336eb84c6566fb
Eric William Dolan <eisforinnovate@gmail.com>
2015-08-19 13:40:10 -0400

    Initial Commit

---
+++ howManyFingers WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  howManyFingers WatchKit Extension
+//
+//  Created by Yatoomy Grey on 2015-08-19.
+//  Copyright © 2015 Neutun Labs. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
